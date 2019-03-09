3abf323c7b2ee9212603abc230a55148323abe66
makotosd <mouri.makoto@gmail.com>
2019-03-09 22:21:28 +0900

    関数化(?)したpredict.pyをshow_predict.pyから呼ぶように改造、jupyter notebook上での動作も確認済み。

--- show_predict.py
+++ show_predict.py
@@ -3,20 +3,31 @@
 
 #######################################################################
 # jupyter-notebook上で動かす。真の値と、予測値をグラフ化します。
-# このスクリプトの実行の前に、predict.pyを実行すること。
-
 #######################################################################
 ## list 16
 # インポート＆実行済みの場合、以下の3行はなくてもよい
+import sys
 import pandas as pd
 import cufflinks as cf
+import predict
+import merge_companies
 cf.go_offline()
 
-# 正解データと予測データ
-correct_data = dataset[dataset.series_data.index >= '2007'].series_data
-predict_data = predict_air_quality
 
-for feature in air_quality.columns:
+#################################################
+#  予測を実行
+#################################################
+ccs = sys.argv
+ccs.pop(0)  # 先頭(script名)を削除
+input_dataset = merge_companies.merge_companies(ccs)
+predict_dataset = predict.predict(input_dataset)
+
+
+# 正解データと予測データ
+correct_data = input_dataset[input_dataset.index >= '2007']
+predict_data = predict_dataset
+
+for feature in predict_dataset.columns:
     plot_data = pd.DataFrame({
         'correct': correct_data[feature],
         'predicted': predict_data[feature]
