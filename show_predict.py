5414adb514926a3f4030e2a2d0a08040f202fc71
makotosd <mouri.makoto@gmail.com>
2019-03-10 13:57:45 +0900

    argparseを導入。 --output を使うと、predict.pyの結果(predict)を出力することが出来る。 また、--inputを使うと、生成済みの結果を読み込むことで、予測を動かさずに予測値をDataFrameに取り込む。

--- show_predict.py
+++ show_predict.py
@@ -2,31 +2,45 @@
 # -*- coding: Shift_JIS -*-
 
 #######################################################################
-# jupyter-notebook上で動かす。真の値と、予測値をグラフ化します。
+#  jupyter notebook上で動かす。真の値と、予測値をグラフ化します。
 #######################################################################
-## list 16
 # インポート＆実行済みの場合、以下の3行はなくてもよい
-import sys
 import pandas as pd
 import cufflinks as cf
+import argparse
 import predict
 import merge_companies
-cf.go_offline()
+
+# arg パーサの生成
+parser = argparse.ArgumentParser(description='予測値と真値の比較、保存、可視化')
+
+# オプション群の設定
+parser.add_argument('--cc', nargs='*', help='company code')
+parser.add_argument('--output', help='予測値と真値の結果(csv)の出力。可視化は行わない。')
+parser.add_argument('--input', help='予測値と真値の結果(csv)の入力。予測は行わない。')
+
+args = parser.parse_args()  # 引数の解析を実行
 
 
-#################################################
+# 真値の読み込み
+input_dataset = merge_companies.merge_companies(args.cc)
+
 #  予測を実行
-#################################################
-ccs = sys.argv
-ccs.pop(0)  # 先頭(script名)を削除
-input_dataset = merge_companies.merge_companies(ccs)
-predict_dataset = predict.predict(input_dataset)
+if args.input is None:  # 読み込みファイルの指定がない　＝　予測の実施
+    predict_dataset = predict.predict(input_dataset)
+
+    if args.output is not None:  # 書き込みファイルの指定がある ＝ ファイルを書く
+        predict_dataset.to_csv(str(args.output))
+
+else:
+    predict_dataset = pd.read_csv(args.input, index_col=0)
 
 
 # 正解データと予測データ
 correct_data = input_dataset[input_dataset.index >= '2007']
 predict_data = predict_dataset
 
+cf.go_offline()  # plotlyをオフラインで実行。
 for feature in predict_dataset.columns:
     plot_data = pd.DataFrame({
         'correct': correct_data[feature],
@@ -37,12 +51,3 @@ for feature in predict_dataset.columns:
     )
     plot_data['layout']['paper_bgcolor'] = '#FFFFFF'
     plot_data.iplot()
-    
-    
-    
-    
-    
-    
-    
-    
-    
