7dc4549df6b4d489670feb6613c18352a3e21fb9
makotosd <mouri.makoto@gmail.com>
2019-03-30 14:49:02 +0900

    train.py側にname_scopeをつけたので、評価側にも追加。

--- show_predict.py
+++ show_predict.py
@@ -13,7 +13,7 @@ import merge_companies
 import TimeSeriesDataSet
 
 
-def rnn_predict(input_dataset, current_time, train_mean, train_std, prediction, x):
+def rnn_predict(input_dataset, current_time, train_mean, train_std, prediction, x, sess):
     # 標準化
     previous = input_dataset.tail(SERIES_LENGTH).standardize(mean=train_mean, std=train_std)
     # 予測対象の時刻
@@ -21,7 +21,7 @@ def rnn_predict(input_dataset, current_time, train_mean, train_std, prediction, 
 
     # 予測
     batch_x = previous.as_array()
-    predict_data = prediction.eval({x: batch_x})
+    predict_data = prediction.eval({x: batch_x}, session=sess)
 
     # 結果のデータフレームを作成
     df_standardized = pd.DataFrame(predict_data, columns=TARGET_FEATURE, index=[predict_time])
@@ -32,14 +32,15 @@ def rnn_predict(input_dataset, current_time, train_mean, train_std, prediction, 
 #############################################################
 def predict(stock_merged_cc, company_codes, features, quote, target_feature):
     # 不要列の除去
-    target_columns = quote
-    for cc in company_codes:
-        for feature in features:
-            cc_f = cc + '_' + feature
-            target_columns.append(cc_f)
-    dataset = TimeSeriesDataSet.TimeSeriesDataSet(stock_merged_cc[target_columns])
-    train_dataset = dataset['2001': '2016']  # 2005年分をトレーニングデータにする。
-    test_dataset = dataset['2016': ]
+    # target_columns = quote
+    # for cc in company_codes:
+    #     for feature in features:
+    #         cc_f = cc + '_' + feature
+    #         target_columns.append(cc_f)
+    # dataset = TimeSeriesDataSet.TimeSeriesDataSet(stock_merged_cc[target_columns])
+    dataset = TimeSeriesDataSet.TimeSeriesDataSet(stock_merged_cc)
+    train_dataset = dataset['2001': '2016']
+    test_dataset = dataset['2017': ]
 
     # パラメーター
     # 学習時間長
@@ -57,11 +58,12 @@ def predict(stock_merged_cc, company_codes, features, quote, target_feature):
     TARGET_FEATURE = target_feature
     TARGET_FEATURE_COUNT = len(TARGET_FEATURE)
 
-    # 入力（placeholderメソッドの引数は、データ型、テンソルのサイズ）
-    # 訓練データ
-    x = tf.placeholder(tf.float32, [None, SERIES_LENGTH, FEATURE_COUNT])
-    # 教師データ
-    y = tf.placeholder(tf.float32, [None, TARGET_FEATURE_COUNT])
+    with tf.name_scope('input'):  # tensorboard用
+        # 入力（placeholderメソッドの引数は、データ型、テンソルのサイズ）
+        # 訓練データ
+        x = tf.placeholder(tf.float32, [None, SERIES_LENGTH, FEATURE_COUNT])
+        # 教師データ
+        y = tf.placeholder(tf.float32, [None, TARGET_FEATURE_COUNT])
 
     # 標準化
     train_mean = train_dataset.mean()
@@ -70,40 +72,42 @@ def predict(stock_merged_cc, company_codes, features, quote, target_feature):
     #######################################################################
     # RNNセルの作成
     print(RNN)
-    if RNN == 'BasicRNNCell':
-        print('BasicRNNCell')
-        cell = tf.nn.rnn_cell.BasicRNNCell(NUM_OF_NEURON)
-        initial_state = cell.zero_state(tf.shape(x)[0], dtype=tf.float32)
-        outputs, last_state = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state, dtype=tf.float32)
-    elif RNN == 'BasicLSTMCell':
-        print('BasicLSTMCell')
-        cell = tf.nn.rnn_cell.BasicLSTMCell(NUM_OF_NEURON)
-        initial_state = cell.zero_state(tf.shape(x)[0], dtype=tf.float32)
-        outputs, last_state = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state, dtype=tf.float32)
-    else:
-        print('No RNN Cell')
-        exit(0)
+    with tf.name_scope('RNN'):  # tensorboard用
+        if RNN == 'BasicRNNCell':
+            print('BasicRNNCell')
+            cell = tf.nn.rnn_cell.BasicRNNCell(NUM_OF_NEURON)
+            initial_state = cell.zero_state(tf.shape(x)[0], dtype=tf.float32)
+            outputs, last_state = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state, dtype=tf.float32)
+        elif RNN == 'BasicLSTMCell':
+            print('BasicLSTMCell')
+            cell = tf.nn.rnn_cell.BasicLSTMCell(NUM_OF_NEURON)
+            initial_state = cell.zero_state(tf.shape(x)[0], dtype=tf.float32)
+            outputs, last_state = tf.nn.dynamic_rnn(cell, x, initial_state=initial_state, dtype=tf.float32)
+        else:
+            print('No RNN Cell')
+            exit(0)
 
-    # 全結合
-    # 重み
-    w = tf.Variable(tf.zeros([NUM_OF_NEURON, TARGET_FEATURE_COUNT]))
-    # バイアス
-    b = tf.Variable([0.1] * TARGET_FEATURE_COUNT)
-    # 最終出力（予測）
-    if RNN == 'BasicRNNCell':
-        prediction = tf.matmul(last_state, w) + b
-    elif RNN == 'BasicLSTMCell':
-        prediction = tf.matmul(last_state[-1], w) + b
+    with tf.name_scope('prediction'):  # tensorboard用
+        # 全結合
+        # 重み
+        w = tf.Variable(tf.zeros([NUM_OF_NEURON, TARGET_FEATURE_COUNT]))
+        # バイアス
+        b = tf.Variable([0.1] * TARGET_FEATURE_COUNT)
+        # 最終出力（予測）
+        if RNN == 'BasicRNNCell':
+            prediction = tf.matmul(last_state, w) + b
+        elif RNN == 'BasicLSTMCell':
+            prediction = tf.matmul(last_state[1], w) + b
 
     ##
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
 
     # restore
-    saver = tf.train.Saver()
-    cwd = os.getcwd()
     if os.name == 'nt':
-        saver.restore(sess, cwd + "\\model.ckpt")
+        ckpt = tf.train.get_checkpoint_state('./')
+        saver = tf.train.Saver()
+        saver.restore(sess, ckpt.model_checkpoint_path)
     else:
         saver.restore(sess, cwd + "/model.ckpt")
 
@@ -113,7 +117,8 @@ def predict(stock_merged_cc, company_codes, features, quote, target_feature):
                                      current_time,
                                      train_mean, train_std,
                                      prediction,
-                                     x)
+                                     x,
+                                     sess)
 
         predict_dataset = predict_dataset.append(predict_result)
 
@@ -134,8 +139,8 @@ if __name__ == "__main__":
     parser.add_argument('--cc', nargs='*', help='company code')
     parser.add_argument('--output', help='予測値と真値の結果(csv)の出力。可視化は行わない。')
     parser.add_argument('--input', help='予測値と真値の結果(csv)の入力。予測は行わない。')
-    parser.add_argument('--feature', nargs='*', help='[open, close, high, low, volume, highopen]',
-                        default=['open', 'close', 'high', 'low', 'highopen'])
+    parser.add_argument('--feature', nargs='*', help='[open, close, high, low, volume]',
+                        default=['open', 'close', 'high', 'low', 'volume'])
     parser.add_argument('--quote', nargs='*', help='[USD, EUR]', default=[])
     parser.add_argument('--target_feature', nargs='*', help='[6702_close, 6702_low], default=[]')
     parser.add_argument('--rnn', nargs=1, help='[BasicLSTMCell|BasicRNNCell]', default='BasicRNNCell')
