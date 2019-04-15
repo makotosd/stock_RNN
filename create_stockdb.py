1a012afbb441c4d747167391b26a6002efdca27d
M M <zzz@zzz.com>
2019-04-16 06:24:08 +0900

    localなmysql-serverにstock_cc_year/*を突っ込む。

--- create_stockdb.py
+++ create_stockdb.py
@@ -9,6 +9,10 @@ import os
 import pandas as pd
 import sqlalchemy as sa
 
+#url = 'mysql+pymysql://root@192.168.1.11:3306/stockdb'
+#url = 'mysql+pymysql://root:murano2002@localhost:3306/stockdb'
+url = 'mysql+pymysql://stock@localhost:3306/stockdb'
+
 def read_stock_csv(csv_file):
     cc = csv_file[23:27]
     year = csv_file[31:35]
@@ -22,16 +26,23 @@ def create_mysqldb():
     if not os.path.exists(dirname):
         exit(-1)
 
-    for file in os.listdir(dirname):
-        print("########")
-        print("## ", file)
-        print("########")
+    files = os.listdir(dirname)
+    #files = ['stocks_2046_1d_2015.csv']
+    i = 0
+    for file in files:
+        print("###################### ", file, i, len(files))
         cc, year, data = read_stock_csv(csv_file=dirname+file)
 
         table_name = 'stocktable_%s' % (cc)
-        url = 'mysql+pymysql://root@192.168.1.11:3306/stockdb'
         engine = sa.create_engine(url, echo=True)
-        data.to_sql(table_name, engine, index=False, if_exists='append')
+        try:
+            data.to_sql(table_name, engine, index=False, if_exists='append')
+        except sa.exc.InternalError as e:
+            print("###################################### catch InternalError: ", e)
+        else:
+            pass  # 成功
+
+        i = i+1
 
 if __name__ == "__main__":
     create_mysqldb()
