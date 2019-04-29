2cd225182e4f618cd05751522e15aafe5a06f831
M M <zzz@zzz.com>
2019-04-29 18:24:21 +0900

    --show_inputと--do_simulationを起動引数に追加。 --show_inputが指定されたときだけ、入力データの可視化を行う。 --do_simualationが指定されたときだけ、モデルのインスタンス化、およびpredictの実行や売買のシミュレーションを行い、可視化する。

--- show_predict.py
+++ show_predict.py
@@ -26,6 +26,11 @@ def predict(dataset, model):
     return predict_dataset
 
 ###########################################################
+def simulation(correct, predict):
+    # TODO: 中身を作る。
+    return pd.DataFrame()
+
+###########################################################
 #   main
 ###########################################################
 RNN = ""
@@ -36,14 +41,11 @@ if __name__ == "__main__":
 
     # オプション群の設定
     parser.add_argument('--cc', nargs='*', help='company code')
-    parser.add_argument('--output', help='予測値と真値の結果(csv)の出力。可視化は行わない。')
-    parser.add_argument('--input', help='予測値と真値の結果(csv)の入力。予測は行わない。')
-    parser.add_argument('--feature', nargs='*', help='[open, close, high, low, volume]',
-                        default=['open', 'close', 'high', 'low', 'volume'])
-    parser.add_argument('--quote', nargs='*', help='[USD, EUR]', default=[])
     parser.add_argument('--target_feature', help='6702_close', default='')
     parser.add_argument('--rnn', help='[BasicLSTMCell|BasicRNNCell]', default='BasicRNNCell')
     parser.add_argument('--num_of_neuron', help='60', default='60', type=int)
+    parser.add_argument('--show_input', action='store_true', default=False)
+    parser.add_argument('--do_simulation', action='store_true', default=False)
 
     args = parser.parse_args()  # 引数の解析を実行
 
@@ -67,72 +69,70 @@ if __name__ == "__main__":
     #######################################################################
     # RNN Modelの生成
     #######################################################################
-    # 特徴量数
-    FEATURE_COUNT = dataset.feature_count
-    # ニューロン数
-    NUM_OF_NEURON = args.num_of_neuron
-    # 最適化対象パラメータ
-    TARGET_FEATURE = args.target_feature
-    TARGET_FEATURE_COUNT = 1
-    # BasicRNNCell or BasicLSTMCell
-    RNN = args.rnn  # rnn[0]
+    if args.do_simulation == True:
+        # 特徴量数
+        FEATURE_COUNT = dataset.feature_count
+        # ニューロン数
+        NUM_OF_NEURON = args.num_of_neuron
+        # 最適化対象パラメータ
+        TARGET_FEATURE = args.target_feature
+        TARGET_FEATURE_COUNT = 1
+        # BasicRNNCell or BasicLSTMCell
+        RNN = args.rnn  # rnn[0]
 
-    #######################################################################
-    model = Model.Model(dataset=dataset, series_length=SERIES_LENGTH, feature_count=FEATURE_COUNT,
-                        num_of_neuron=NUM_OF_NEURON, rnn=RNN,
-                        target_feature=TARGET_FEATURE, target_feature_count=TARGET_FEATURE_COUNT)
+        #######################################################################
+        model = Model.Model(dataset=dataset, series_length=SERIES_LENGTH, feature_count=FEATURE_COUNT,
+                            num_of_neuron=NUM_OF_NEURON, rnn=RNN,
+                            target_feature=TARGET_FEATURE, target_feature_count=TARGET_FEATURE_COUNT)
+        ##
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
 
-    ##
-    sess = tf.InteractiveSession()
-    sess.run(tf.global_variables_initializer())
+        # restore
+        directory_model = "./model/" + args.target_feature
+        if os.path.exists(directory_model):
+            ckpt = tf.train.get_checkpoint_state(directory_model)
+            saver = tf.train.Saver()
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print('model diretory does NOT exists.: {}'.format(directory_model))
+            exit(0)
 
-    # restore
-    directory_model = "./model/" + args.target_feature
-    if os.path.exists(directory_model):
-        ckpt = tf.train.get_checkpoint_state(directory_model)
-        saver = tf.train.Saver()
-        saver.restore(sess, ckpt.model_checkpoint_path)
-    else:
-        print('model diretory does NOT exists.: {}'.format(directory_model))
-        exit(0)
-
-    ########################################################################
-    #  予測を実行
-    ########################################################################
-    if args.input is None:  # 読み込みファイルの指定がない　＝　予測の実施
+        ########################################################################
+        #  予測を実行
+        ########################################################################
+        predict_dataset = pd.DataFrame()
         predict_dataset = predict(dataset, model)
-
-        if args.output is not None:  # 書き込みファイルの指定がある ＝ ファイルを書く
-            predict_dataset.to_csv(str(args.output))
-
-    else:
-        predict_dataset = pd.read_csv(args.input, index_col=0)
+        simulation_result = simulation(dataset.test_dataset, predict_dataset)
 
     ########################################################################
     #  グラフ化
     ########################################################################
     correct_data = pd.concat([dataset.train_dataset.series_data,
                               dataset.test_dataset.series_data]).drop_duplicates()
-    predict_data = predict_dataset
-
     cf.go_offline()  # plotlyをオフラインで実行。
-    for feature in predict_dataset.columns:
-        plot_data = pd.DataFrame({
-            'correct': correct_data[feature],
-            'predicted': predict_data[feature]
-        }).iplot(
-          asFigure = True,
-          title = feature
-        )
-        plot_data['layout']['paper_bgcolor'] = '#FFFFFF'
-        plot_data.iplot()
 
-    for feature in correct_data.columns:
-        plot_data = pd.DataFrame({
-            'correct': correct_data[feature],
-        }).iplot(
-          asFigure = True,
-          title = feature
-        )
-        plot_data['layout']['paper_bgcolor'] = '#FFFFFF'
-        plot_data.iplot()
+    if(args.do_simulation == True):
+        predict_data = predict_dataset
+
+        for feature in predict_dataset.columns:
+            plot_data = pd.DataFrame({
+                'correct': correct_data[feature],
+                'predicted': predict_data[feature]
+            }).iplot(
+              asFigure = True,
+              title = feature
+            )
+            plot_data['layout']['paper_bgcolor'] = '#FFFFFF'
+            plot_data.iplot()
+
+    if(args.show_input == True):
+        for feature in correct_data.columns:
+            plot_data = pd.DataFrame({
+                'correct': correct_data[feature],
+            }).iplot(
+              asFigure = True,
+              title = feature
+            )
+            plot_data['layout']['paper_bgcolor'] = '#FFFFFF'
+            plot_data.iplot()
